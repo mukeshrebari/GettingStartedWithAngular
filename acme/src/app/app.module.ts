488030b237c5a9a9de85a7d08dd3e147fe5e1cfb
Mukesh Rebari <mukesh.rebari@hotmail.com>
2021-02-03 22:30:26 +0530

    check point

--- acme/src/app/app.module.ts
+++ acme/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { ProductListComponent } from './product-list/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,ProductListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
